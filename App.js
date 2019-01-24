cbd22f79c39940e7979012010244df86619eb775
Mattia <mattiabellotti.dev@gmail.com>
2019-01-24 13:40:40 +0100

    a
    a

--- App.js
+++ App.js
@@ -19,9 +19,9 @@ YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTIm
 import { createStackNavigator } from 'react-navigation';
 
 // export const baseUrl = "http://192.168.137.1:8000/";
-// export const baseUrl = "http://192.168.1.6:8000/";
-//export const baseUrl = "http://drink-web.eu:8000/";
-export const baseUrl = "http://tido.novus.cc/";
+export const baseUrl = "http://192.168.0.104:8000/";
+// export const baseUrl = "http://drink-web.eu:8000/";
+// export const baseUrl = "http://tido.novus.cc/";
 
 export const loginurl = "/api/login";
 export const homeurlweb = "/api/homeweb";
